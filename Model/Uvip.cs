a2b2537d6bddb8d089168ece83a89ed552f3ab26
fangxin <13839870291@users.noreply.github.com>
2018-10-08 09:12:47 +0800

    增加会员功能

---
+++ Model/Uvip.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace Maticsoft.Model
+{
+	/// <summary>
+	/// Uvip:实体类(属性说明自动提取数据库字段的描述信息)
+	/// </summary>
+	[Serializable]
+	public partial class Uvip
+	{
+		public Uvip()
+		{}
+		#region Model
+		private int _id;
+		private string _uname;
+		private string _utel;
+		private string _ucode;
+		private string _uads;
+		private string _usex;
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Id
+		{
+			set{ _id=value;}
+			get{return _id;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Uname
+		{
+			set{ _uname=value;}
+			get{return _uname;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Utel
+		{
+			set{ _utel=value;}
+			get{return _utel;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Ucode
+		{
+			set{ _ucode=value;}
+			get{return _ucode;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Uads
+		{
+			set{ _uads=value;}
+			get{return _uads;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Usex
+		{
+			set{ _usex=value;}
+			get{return _usex;}
+		}
+		#endregion Model
+
+	}
+}
+
---
+++ Model/UvipInfo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Maticsoft.Model
+{
+	/// <summary>
+	/// UvipInfo:实体类(属性说明自动提取数据库字段的描述信息)
+	/// </summary>
+	[Serializable]
+	public partial class UvipInfo
+	{
+		public UvipInfo()
+		{}
+		#region Model
+		private int _id;
+		private int? _uvipid;
+		private decimal? _uqyt;
+		private string _urmk;
+		private DateTime? _utime;
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Id
+		{
+			set{ _id=value;}
+			get{return _id;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public int? UvipId
+		{
+			set{ _uvipid=value;}
+			get{return _uvipid;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public decimal? Uqyt
+		{
+			set{ _uqyt=value;}
+			get{return _uqyt;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Urmk
+		{
+			set{ _urmk=value;}
+			get{return _urmk;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public DateTime? Utime
+		{
+			set{ _utime=value;}
+			get{return _utime;}
+		}
+		#endregion Model
+
+	}
+}
+
